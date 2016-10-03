c087ddea856556cc8a0e89d1f176fc6d79c06231
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2016-10-03 22:51:09 +0200

    try versioning on Frigo find method

--- frigo/src/main/java/org/kj6682/frigo/FrigoRestController.java
+++ frigo/src/main/java/org/kj6682/frigo/FrigoRestController.java
@@ -1,10 +1,16 @@
 package org.kj6682.frigo;
 
+import io.swagger.annotations.ResponseHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -38,6 +44,36 @@ class FrigoRestController {
     List<Frigo> find(@RequestParam(value = "search4me", required = false) String search4me) {
 
         return frigoService.find(search4me);
+
+    }
+
+    /**
+     * In order to use this demo method for vesioning with custom headers, try :
+     *
+     * curl --header "frigo.version:v2" "http://localhost:8081/frigo
+     *
+     *
+     * @param search4me
+     * @return
+     */
+    @GetMapping(value = "/frigo", headers = "frigo.version=v2")
+    ResponseEntity<List<Frigo>> findWithHeaders(@RequestParam(value = "search4me", required = false) String search4me) {
+
+        HttpHeaders headers = new HttpHeaders();
+        List<MediaType> l = new LinkedList<MediaType>();
+
+        headers.set("frigo.version","v2");
+
+        Frigo dummyFrigo = new Frigo();
+        dummyFrigo.setId("*** dummy frigo idem ***");
+        dummyFrigo.setName("This instance is accessible only in version 2");
+        dummyFrigo.setQuantity(0);
+        dummyFrigo.setBestBefore(LocalDate.MAX);
+        dummyFrigo.setSince(LocalDate.now());
+        List<Frigo> frigos = frigoService.find(search4me);
+        frigos.add(dummyFrigo);
+
+        return new ResponseEntity<List<Frigo>>(frigos, headers, HttpStatus.OK);
 
     }
 
